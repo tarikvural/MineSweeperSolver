8d05cfaf2e6dd85f56b249551382d04059d9c2fc
Tarik Vural <tarik.vural@rwth-aachen.de>
2024-07-21 22:00:29 +0200

    started implementing some of the tile detection logic

--- src/GameManager/GameManager.tsx
+++ src/GameManager/GameManager.tsx
@@ -14,6 +14,70 @@ function GameManager({ width, height, resetTrigger, mines }: GameManagerProps) {
     const [flags, setFlags] = useState<boolean[]>([]);
 
 
+    const getRowAndColFromIndex = (index: number): {row: number, col: number} => {
+        return {row: Math.floor(index / width),col: index % width};
+    }
+
+    const getIndexFromRowAndCol = (row: number,col: number): number => {
+        return row * width + col;
+    }
+
+    const countFlagsAround = (index: number) : number => {
+        const colAndRow = getRowAndColFromIndex(index);
+        return countFlagsAroundHelper(colAndRow.row, colAndRow.col); 
+    }
+
+    const countFlagsAroundHelper = (i: number, j: number): number => {
+        let minesAround = 0;
+        
+        let upperEdge:boolean = false, lowerEdge:boolean = false, leftEdge:boolean = false, rightEdge:boolean = false;
+        
+        if(i === 0) upperEdge = true;
+        if(j === 0) leftEdge = true;
+        if(i === height-1) lowerEdge = true;
+        if(j === width-1) rightEdge = true;
+        
+        if(!upperEdge && flags[getIndexFromRowAndCol(i-1,j)]) minesAround++;
+        if(!leftEdge && flags[getIndexFromRowAndCol(i,j-1)]) minesAround++;
+        if(!lowerEdge && flags[getIndexFromRowAndCol(i+1,j)]) minesAround++;
+        if(!rightEdge && flags[getIndexFromRowAndCol(i,j+1)]) minesAround++;
+        if(!upperEdge && !leftEdge && flags[getIndexFromRowAndCol(i-1,j-1)]) minesAround++;
+        if(!upperEdge && !rightEdge && flags[getIndexFromRowAndCol(i-1,j+1)]) minesAround++;
+        if(!lowerEdge && !leftEdge && flags[getIndexFromRowAndCol(i+1,j-1)]) minesAround++;
+        if(!lowerEdge && !rightEdge && flags[getIndexFromRowAndCol(i+1,j+1)]) minesAround++;
+
+        return minesAround;
+    }
+    
+    const countFreeSquaresAround = (index: number): number => {
+        const colAndRow = getRowAndColFromIndex(index);
+        return countFreeSquaresAroundHelper(colAndRow.row, colAndRow.col);
+    }
+
+    // i is row, j is col
+    const countFreeSquaresAroundHelper = (i: number, j: number): number => {
+        let freeSquares = 0;
+        
+        let upperEdge:boolean = false, lowerEdge:boolean = false, leftEdge:boolean = false, rightEdge:boolean = false;
+        
+        if(i === 0) upperEdge = true;
+        if(j === 0) leftEdge = true;
+        if(i === height-1) lowerEdge = true;
+        if(j === width-1) rightEdge = true;
+        
+        if(!upperEdge && tileValues[getIndexFromRowAndCol(i-1,j)]==-1) freeSquares++;
+        if(!lowerEdge && tileValues[getIndexFromRowAndCol(i+1,j)]==-1) freeSquares++;
+        if(!leftEdge && tileValues[getIndexFromRowAndCol(i,j-1)]==-1) freeSquares++;
+        if(!rightEdge && tileValues[getIndexFromRowAndCol(i,j+1)]==-1) freeSquares++;
+        if(!leftEdge && !upperEdge && tileValues[getIndexFromRowAndCol(i-1,j-1)]==-1) freeSquares++;
+        if(!upperEdge && !rightEdge && tileValues[getIndexFromRowAndCol(i-1,j+1)]==-1) freeSquares++;
+        if(!leftEdge && !lowerEdge && tileValues[getIndexFromRowAndCol(i+1,j-1)]==-1) freeSquares++;
+        if(!lowerEdge && !rightEdge && tileValues[getIndexFromRowAndCol(i+1,j+1)]==-1) freeSquares++;
+
+        console.log(freeSquares);
+        return freeSquares;
+    }
+
     useEffect(() => {
         resetGame();
     }, [width, height, resetTrigger,mines]);
@@ -35,7 +99,13 @@ function GameManager({ width, height, resetTrigger, mines }: GameManagerProps) {
     const handleTileRightClicked = (index: number) => {
         setTileValues(prev => {
         const newTileValues = [...prev];
-        newTileValues[index] = prev[index] === -3 ? -1 : -3;
+        if(prev[index] === -3){
+            newTileValues[index] = -1;
+            flags[index] = false;
+        } else {
+            newTileValues[index] = -3;
+            flags[index] = true;
+        }
         return newTileValues;
         });
     };
